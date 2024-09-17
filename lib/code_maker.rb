86fb2f9d9e5162d22614c233ce6a02174dd91010
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-09-17 10:33:22 +0300

    Added CodeMaker initialization

--- lib/code_maker.rb
+++ lib/code_maker.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class CodeMaker
+  ALLOWED_COLORS = %w[R G B Y O P].freeze
+
+  attr_reader :code
+
+  def initialize
+    @code = %w[R G B Y O P].sample(4)
+  end
 end
--- main.rb
+++ main.rb
@@ -3,3 +3,7 @@
 require_relative 'lib/game'
 require_relative 'lib/code_breaker'
 require_relative 'lib/code_maker'
+
+code = CodeMaker.new
+
+p code.code
