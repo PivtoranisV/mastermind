42e94ae2761d425c7c66b370b24eac2b5fb9d0f4
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-09-17 17:41:46 +0300

    Added make_guess method to CodeBreaker class

--- lib/code_breaker.rb
+++ lib/code_breaker.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 class CodeBreaker
+  ALLOWED_COLORS = %w[R G B Y O P].freeze
+
+  def make_guess
+    loop do
+      puts 'Please, enter 4 unique colors from these options:'
+      puts ALLOWED_COLORS.join(', ')
+      guess = gets.chomp.upcase.gsub(/[^A-Z\s]/, ' ').split
+
+      return guess if valid_guess?(guess)
+
+      puts 'Invalid input! Make sure you enter 4 unique colors from the allowed list.'
+    end
+  end
+
+  private
+
+  def valid_guess?(guess)
+    guess.length == 4 && guess.all? { |color| ALLOWED_COLORS.include?(color) } && guess.uniq.length == 4
+  end
 end
--- main.rb
+++ main.rb
@@ -5,7 +5,9 @@ require_relative 'lib/code_breaker'
 require_relative 'lib/code_maker'
 
 code = CodeMaker.new
+guess = CodeBreaker.new
 
 p code.code
 code.exact_match_feedback(%w[R P G O])
 code.color_match_feedback(%w[R P G O])
+p guess.make_guess
